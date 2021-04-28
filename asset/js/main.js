3fa1f8fcd65980b30ceaf40f29b48a141383c257
valeriomatranga <matrangav@gmail.com>
2021-04-28 17:29:57 +0200

    iterazione

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,11 +2,20 @@ var cognome = prompt("Scrivi il tuo cognome!");
 
 var lista = ["rossi","bianchi","martini","verdi","matranga"];
 
+var fuori;
+
 var fuori = lista.includes(cognome);
  
 if (fuori != lista){
     lista.push(cognome);
     lista.sort();
+    console.log('iterazione' + (i + 1));
+    console.log(lista.indexOf(cognome));
  }
  
- console.log (lista);
+ console.log (lista) ; 
+ 
+ 
+/*  for (i = 0; 1 < lista.length; i++){
+    console.log(lista);
+ } */
