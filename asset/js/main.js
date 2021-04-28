fcaf725d508e581dec3a6314d21249d6046d6b28
valeriomatranga <matrangav@gmail.com>
2021-04-28 16:19:44 +0200

    contatore

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,10 +2,14 @@ var cognome = prompt("Scrivi il tuo cognome!");
 
 var lista = ["rossi","bianchi","martini","verdi","matranga"];
 
+ 
 for (i = 1; i < lista.length; i++){
     
-    if (cognome != lista){
-        var total = lista.push("vale");
-        console.log(lista);  
+    if (cognome != lista[i]){
+        lista.push(cognome);
+        console.log(lista);
+        
     }
 }
+
+ 
